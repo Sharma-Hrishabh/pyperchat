dc247c821e674ed7644e079d5fa24b3d5363aaa1
Sharma-Hrishabh <30hrs1999@gmail.com>
2018-12-14 16:23:00 +0530

    websocket.receive fully functional

--- chat/consumers.py
+++ chat/consumers.py
@@ -15,11 +15,15 @@ class ChatConsumer(AsyncConsumer):
         other_user=self.scope['url_route']['kwargs']['username']
         me=self.scope['user']
         print(other_user,me)
-        thread_obj=await self.get_thread(me,other_user)
+        thread_obj= await self.get_thread(me,other_user)
         print(thread_obj)
 
-    async def websocket_recieve(self,event):
-        # when a message is recieved
+        await self.send({
+        "type":"websocket.send",
+        "text":"helloooo"
+        })
+
+    async def websocket_receive(self,event):
         print("recieved",event)
 
     async def websocket_disconnect(self,event):
